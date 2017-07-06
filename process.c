e8d35e79bdfee56fb7957486273e248c457b94f0
fjunk <fajunk001@gmail.com>
2017-07-06 15:37:03 +0200

    tested reading of GET-request

--- process.c
+++ process.c
@@ -15,16 +15,46 @@
  */
 
 /* strstr find substring 
- * move last 3 chars to beginning of new read */
+ * move last 3 chars to beginning of new read 
+ * buffersize = 1003
+ * terminate buffer with \0  */
 
-char *LINE_BREAK = "\r\n";
+const char *LINE_BREAK = "\r\n";
+const char *DOUBLE_LINE_BREAK ="\r\n\r\n";
 
 
 
 
-void process_request(int fd_in, int fd_out) {
+void read_request(int read_fd){
 
+    enum { BUFFSIZE=1000 };
+    char buffer[BUFFSIZE];
+    char *str_end;
+    int did_read;
+
+    while (1) {
+
+        did_read = read(read_fd, buffer, BUFFSIZE);
+
+        if (did_read == 0) {
+
+            str_end = strstr(buffer, LINE_BREAK);
+            str_end = '\0';
+            printf("%s\n", buffer);
+
+            break;
+        } else if (did_read < 0) {
+            perror("readerror");
+        }
+
+    }
 
 }
 
 
+void main() {
+
+    int read_fd = open("input.txt", O_RDONLY);
+    read_request(read_fd);
+
+}
